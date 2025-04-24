04ccddc078ff8dc480e92a14fa2fcd7ac4d01036
AlyaSyiraFahleavi <alyasyirafahlevi@gmail.com>
2025-04-24 14:07:46 +0700

    Implementasi Template dan Boostrap

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Halaman utama
 $routes->get('/', 'Home::index');
+
+// Halaman About
+$routes->get('/about', 'Pages::about');
+
+// Halaman How
+$routes->get('/how', 'Pages::how');
---
+++ app/Views/scripts.php
@@ -0,0 +1,28 @@
+<script type="text/javascript" src="<?= base_url('js/jquery-3.4.1.min.js') ?>"></script>
+<script type="text/javascript" src="<?= base_url('js/bootstrap.js') ?>"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+
+</script>
+<!-- owl carousel script 
+    -->
+<script type="text/javascript">
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 0,
+        navText: [],
+        center: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+</script>
+<!-- end owl carousel script -->
